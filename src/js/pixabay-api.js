6f6a73ad66464f43a7d7c690536e8d29c6e54cc1
OksanaBode <ksiushka1@gmail.com>
2025-06-14 11:00:43 +0200

    rewrite for async/await

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,19 @@
+import axios from 'axios';
 const PIXABAY_API_KEY = '50718626-0f15284cd54e6013b04c57504';
-const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export const fetchImages = searchedQuery => {
-  const urlParams = new URLSearchParams({
-    key: PIXABAY_API_KEY,
-    q: searchedQuery,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 9,
-  });
-  return fetch(`${BASE_URL}?${urlParams.toString()}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+export const fetchImages = (searchedQuery, currentPage, perPage) => {
+  const axiosOptions = {
+    params: {
+      key: PIXABAY_API_KEY,
+      q: searchedQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: currentPage,
+      per_page: perPage,
+    },
+  };
+
+  return axios.get('', axiosOptions);
 };
