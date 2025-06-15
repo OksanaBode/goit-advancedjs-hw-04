bac68f7ce4f3a4023521d489de8e7b6466d49d71
OksanaBode <ksiushka1@gmail.com>
2025-06-15 16:14:57 +0200

    corrected pixabay js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const PIXABAY_API_KEY = '50718626-0f15284cd54e6013b04c57504';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export const fetchImages = (searchedQuery, currentPage, perPage) => {
+export const fetchImages = async (searchedQuery, currentPage, perPage) => {
   const axiosOptions = {
     params: {
       key: PIXABAY_API_KEY,
@@ -15,5 +15,11 @@ export const fetchImages = (searchedQuery, currentPage, perPage) => {
     },
   };
 
-  return axios.get('', axiosOptions);
+  try {
+    const response = await axios.get('', axiosOptions);
+    return response;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
 };
